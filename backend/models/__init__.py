e8167e55e31ff9b645bad6783e3a4789fc694aa0
Santhoshkn05 <Santhoshkumarnaik21@gmail.com>
2025-09-19 17:26:59 +0530

    Add files via upload

---
+++ backend/models/__init__.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from .user import User
+from .project import Project
+from .task import Task
+from .habit import Habit
+from .todo import Todo
+from .project_updates import ProjectUpdate
