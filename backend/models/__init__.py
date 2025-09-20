2413d5e803b1e2b7a3f439698ab15c38cb977dbf
Santhoshkn05 <Santhoshkumarnaik21@gmail.com>
2025-09-20 13:31:59 +0530

    Delete backend/models/__init__.py

--- backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from backend.models.user import User
-from .project import Project
-from .task import Task
-from .habit import Habit
-from .todo import Todo
-from .project_updates import ProjectUpdate
