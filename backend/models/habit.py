b8d115892fc7860847e7998d9019f8550d0b3bde
Santhoshkn05 <Santhoshkumarnaik21@gmail.com>
2025-09-20 22:38:08 +0530

    Add files via upload

---
+++ backend/models/habit.py
@@ -0,0 +1,12 @@
+from db import db
+
+class Habit(db.Model):
+    __tablename__ = 'habits'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True) 
+    status = db.Column(db.String(50), nullable=False, default='pending')
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship("User", back_populates="habits")
