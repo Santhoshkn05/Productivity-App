35b74f6e24c3b2cec051318d33d7dcf238a76cfe
Santhoshkn05 <Santhoshkumarnaik21@gmail.com>
2025-09-20 12:31:23 +0530

    Delete backend/models/attachment.py

--- backend/models/attachment.py
+++
@@ -1,7 +0,0 @@
-from config import db
-
-class Attachment(db.Model):
-    __tablename__ = "attachments"
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)
-    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
